48987a4a9a564e7d880ef8bfdd58a8fa0fd26eb6
Liam Goodchild <475896@chrome.sussexdowns.ac.uk>
2018-11-28 14:34:29 +0000

    Creating Flashlight

---
+++ College Game Project/Assets/Scripts/Controller/Flashlight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+
+    private bool flashlightEnabled;
+    public GameObject flashlight;
+    public GameObject lightObj;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            flashlightEnabled =! flashlightEnabled;
+
+        if (flashlightEnabled)
+            flashlight.SetActive(true);
+        else
+            flashlight.SetActive(false);
+    }
+}
